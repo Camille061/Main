1f20affd42bc74308d30fc43d53c132094ed003a
Your Name <you@example.com>
2025-10-12 23:10:55 +0700

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 let line = document.getElementById("contract-box");
 
-// ใช้ 'click' เป็น string
 line.addEventListener("click", function() {
-    alert   ("ไม่ให้จ้า");
+    alert("ไม่ให้จ้า");
+});
+
+let discord = document.getElementById("discord-box");
+
+discord.addEventListener("click", function() {
+     e.preventDefault();
+    alert("ไม่ให้จ้า");
+       window.location.href = 'https://discord.com/users/1405831980473389126"';
 });
 
 
